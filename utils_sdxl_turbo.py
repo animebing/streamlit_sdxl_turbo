1d6ea76bacd23eb467cfbde510d3993c28a09c1c
animebing <yanbing_dong@hotmail.com>
2023-12-01 00:09:43 +0800

    sdxl turbo

---
+++ utils_sdxl_turbo.py
@@ -0,0 +1,13 @@
+from diffusers import AutoPipelineForText2Image
+import torch
+
+
+def generate_image(text):
+    image = pipe(prompt=text, num_inference_steps=1, guidance_scale=0.0).images[0]
+    return image
+
+
+model_name = 'stabilityai/sdxl-turbo' # or local path
+pipe = AutoPipelineForText2Image.from_pretrained(model_name, torch_dtype=torch.float16, variant="fp16")
+pipe.to("cuda")
+pipe(prompt='a cat', num_inference_steps=1, guidance_scale=0.0) # warmup
